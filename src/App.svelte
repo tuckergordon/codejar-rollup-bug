b0670f394a483647f5356fe15d8b7700b6260349
Tucker Gordon <gordon_tucker@bah.com>
2022-06-16 07:56:51 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Codejar from "./Codejar.svelte";
+</script>
+
+<Codejar />
+
+<style></style>
---
+++ src/Codejar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { CodeJar } from "codejar";
+  import { onMount } from "svelte";
+
+  const highlight = (editor: HTMLElement) => {
+    const code = editor.textContent;
+    // Do something with code and set html.
+    editor.innerHTML = code;
+  };
+
+  onMount(() => {
+    const jar = CodeJar(document.querySelector(".editor"), highlight);
+  });
+</script>
+
+<div class="editor" />
+
+<style>
+  .editor {
+    height: 400px;
+    width: 400px;
+    background: lightblue;
+  }
+</style>
